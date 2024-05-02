78f5e2bc5a8e614db8582f4d2555b692c57a8e48
Anurag Modi <modi.anurag1992@gmail.com>
2024-05-02 17:55:43 +0530

    Update urls.py

--- swing_trader/urls.py
+++ swing_trader/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('', include('signals.urls')),
     path("admin/", admin.site.urls),
     path('signals/', include('signals.urls')),
 ]
