d9c0464bebd497ea89df5b45b8ecd2b312463ea9
Anurag Modi <modi.anurag1992@gmail.com>
2024-05-03 18:33:45 +0530

    Update settings.py

--- swing_trader/settings.py
+++ swing_trader/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-3ql925ey@3+x@bo23=&h&xvj*(l!+63z90#kqdj**^hc#7@zwb
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1']
+ALLOWED_HOSTS = ['anurag3753.pythonanywhere.com','.vercel.app', '127.0.0.1']
 
 
 # Application definition
