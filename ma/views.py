e0e4e3ec9f515b23a1a9d8785a28ffbd49dd03d3
Anurag Modi <modi.anurag1992@gmail.com>
2024-12-31 18:53:36 +0530

    Update views.py to remove duplicates

--- ma/views.py
+++ ma/views.py
@@ -1,4 +1,7 @@
 from django.views.generic import ListView
+from django.db.models import F, Subquery, OuterRef, Case, When, BooleanField
+from django.utils import timezone
+from datetime import timedelta
 from .models import StockSignal
 
 class StockSignalListView(ListView):
@@ -10,3 +13,28 @@ class StockSignalListView(ListView):
     def get_ordering(self):
         ordering = self.request.GET.get('ordering', 'date')
         return ordering
+    
+    def get_queryset(self):
+        queryset = super().get_queryset()
+
+        # Get today's date
+        today = timezone.now().date()
+
+        # Create a subquery to filter out duplicates
+        unique_signals_subquery = StockSignal.objects.filter(
+            symbol=OuterRef('symbol'),
+            date=OuterRef('date'),
+            price=OuterRef('price'),
+        ).values('pk')[:1]
+
+        # Annotate the queryset with the unique signal and a flag for new signals
+        queryset = queryset.annotate(
+            unique_signal=Subquery(unique_signals_subquery),
+            is_new=Case(
+                When(date__gte=today - timedelta(days=7), then=True),  # Check if added in the last 3 days
+                default=False,
+                output_field=BooleanField()
+            )
+        ).filter(pk=F('unique_signal'))
+
+        return queryset
